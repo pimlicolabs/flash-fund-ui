9d9ab4fa70511cc92e581f73a040c808b4ad41f2
Sergey Potekhin <sergey@pimlico.io>
2024-11-15 12:53:55 +0700

    Update metadata

---
+++ src/components/connect-wallet.tsx
@@ -0,0 +1,13 @@
+// import { cn } from "@/lib/utils";
+
+export const ConnectWallet = () => {
+	return (
+		<div className={"flex flex-col items-center justify-center gap-4 py-12"}>
+			<p className="text-lg text-muted-foreground">
+				Please connect your wallet first 👛
+			</p>
+		</div>
+	);
+};
+
+export default ConnectWallet;
